19f90da7db46ac153ad90d918f1235c65ce18927
Rodrigo Alves Tenório <rodten23@gmail.com>
2024-12-17 02:07:20 -0300

    :sparkles: feat: Criação da rota users e criação e implementação do seu  schemas.

--- fastapi_du_zero/app.py
+++ fastapi_du_zero/app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_du_zero.schemas import Message
+from fastapi_du_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
@@ -10,3 +10,8 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
