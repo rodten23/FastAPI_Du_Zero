92a52a31d374ea67c1fc3afd04e349bfe1de62d6
rodten23 <rodten23@gmail.com>
2025-05-19 16:43:33 -0300

    :test_tube: test: Evolução do teste para o banco de dados.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,28 @@
-from fastapi_du_zero.models import User
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+
+from fastapi_du_zero.models import User, table_registry
 
 
-def test_create_user(client):
-    user = User(username='Rodrigo',
-                email='rodrigo@encadernacao.com',
-                password='vamosla')
+def test_create_user():
+    # Cria o banco de dados temporário em memória para o teste.
+    # O parâmetro :memory: não funciona para todos os tipos de DB.
+    engine = create_engine('sqlite:///:memory:')
 
-    assert user.password == 'vamosla'
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        user = User(
+            username='Rodrigo',
+            email='rodrigo@encadernacao.com',
+            password='vamosla',
+        )
+
+        session.add(user)
+        session.commit()
+
+        result = session.scalar(
+            select(User).where(User.email == 'rodrigo@encadernacao.com')
+        )
+
+    assert result.username == 'Rodrigo'
