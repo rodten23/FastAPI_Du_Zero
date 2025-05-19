3d8d45fea98c86457fd6899348a9370eddcde5f1
rodten23 <rodten23@gmail.com>
2025-05-19 15:56:59 -0300

    style: Ajusta na formatação do código no models.py e test_models.py

--- fastapi_du_zero/models.py
+++ fastapi_du_zero/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from sqlalchemy import func
-from sqlalchemy.orm import registry, Mapped, mapped_column
 
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
 
@@ -10,8 +10,9 @@ table_registry = registry()
 class User:
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(init = False, primary_key = True)
-    username: Mapped[str] = mapped_column(unique = True)
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
-    email: Mapped[str] = mapped_column(unique = True)
-    created_at: Mapped[datetime] = mapped_column(init = False, server_default = func.now())
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(init=False,
+                                                 server_default=func.now())
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,8 @@ from fastapi_du_zero.models import User
 
 
 def test_create_user(client):
-    user = User(username = 'Rodrigo', email = 'rodrigo@encadernacao.com', password = 'vamosla')
+    user = User(username='Rodrigo',
+                email='rodrigo@encadernacao.com',
+                password='vamosla')
 
     assert user.password == 'vamosla'
