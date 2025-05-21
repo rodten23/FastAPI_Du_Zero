1484f463031bef0758d49bcf952ab9293bff0b46
rodten23 <rodten23@gmail.com>
2025-05-20 21:40:01 -0300

    :test_tube: test: Adequação do teste para o banco de dados para lidar com eventos e burlar o created-at.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from fastapi_du_zero.app import app
@@ -26,3 +29,21 @@ def session():
     # Até aqui é o Arrange do teste.
     # E o drop.all abaixo é o "Tear down" do teste.
     table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2025, 5, 20)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,20 +1,29 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fastapi_du_zero.models import User
 
 
-def test_create_user(session):
-    user = User(
-        username='Rodrigo',
-        email='rodrigo@encadernacao.com',
-        password='vamosla',
-    )
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='Rodrigo',
+            email='rodrigo@encadernacao.com.br',
+            password='vamosla',
+        )
 
-    session.add(user)
-    session.commit()
+        session.add(new_user)
+        session.commit()
 
     result = session.scalar(
-        select(User).where(User.email == 'rodrigo@encadernacao.com')
+        select(User).where(User.email == 'rodrigo@encadernacao.com.br')
     )
 
-    assert result.username == 'Rodrigo'
+    assert asdict(result) == {
+        'id': 1,
+        'username': 'Rodrigo',
+        'password': 'vamosla',
+        'email': 'rodrigo@encadernacao.com.br',
+        'created_at': time,
+    }
